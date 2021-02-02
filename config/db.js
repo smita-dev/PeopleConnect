be64822995d8c7bbd0376574d56510fb542660ce
smita patil <smitakpatil26@gmail.com>
2021-02-02 23:15:44 +0530

    connected to database

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const config=require('config');
+const db=config.get('url');
+
+const dbConnect= async ()=>{
+    try{
+       await mongoose.connect(db,
+        {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+        });
+        console.log('mongodb connected');
+    }
+    catch(err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+    
+}
+
+module.exports=dbConnect;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 //
-const express=require('express');4
-
+const express=require('express');
+const connectDb=require('./config/db')
+//const student=require('./routes/api/student')
 const app=express();
 
+connectDb();
 const PORT=process.env.PORT||8000;
 
 app.listen(PORT,()=>{
     console.log(`server is listening at ${PORT}`)
 })
 
+
+
+app.use('/api/student',require('./routes/api/student'));
 app.get('/',(req,res)=>{
     //console.log()
     res.send("get the data");
