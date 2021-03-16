b689ac84d2d491206db681ad3d9d801e89d48c78
smita patil <smitakpatil26@gmail.com>
2021-03-16 08:59:38 +0530

    input validation using body express validator

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,20 @@
 const express=require('express');
-
+const {body,validationResult}=require('express-validator')
 const router=express.Router();
 
 //@route   GET api/user
 //@Desc    test route
 //@access  public
 
-router.post('/',(req,res)=>{
+router.post('/',
+body('Name').isEmail(),
+body('Password').isLength({min:5})
+,(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors:errors.array()})
+    }
     console.log("in user router");
     console.log(req.body);
     res.send("in user api")
