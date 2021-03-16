7c9f728cafd1e8a9112d3d530d262af6fac83c47
smita patil <smitakpatil26@gmail.com>
2021-03-16 08:40:17 +0530

    added middleware to get data from request

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 //
 const express=require('express');
 const connectDb=require('./config/db')
-//const student=require('./routes/api/student')
-const app=express();
 
+const app=express();
 connectDb();
+
+//initialising middleware
+app.use(express.json({extended:false}))
+app.use('/api/user',require('./routes/api/user'));
+
+app.get('/',(req,res)=>{
+    //console.log()
+    res.send("get the data");
+})
+
 const PORT=process.env.PORT||8000;
 
 app.listen(PORT,()=>{
     console.log(`server is listening at ${PORT}`)
-})
-
-
-
-app.use('/api/student',require('./routes/api/student'));
-app.get('/',(req,res)=>{
-    //console.log()
-    res.send("get the data");
 })
